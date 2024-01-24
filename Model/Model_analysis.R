527352c7921d33359d3ce6a55355cbe0a90e1cd4
Umesh Patil <umesh.patil@gmail.com>
2024-01-24 13:44:05 +0100

    Update model analysis script

--- Model/Model_analysis.R
+++ Model/Model_analysis.R
@@ -1,7 +1,6 @@
 
 library(tidyverse)
 
-
 # Function that returns the first index of the column in 'data' where 
 # 'seq_len' no. of consecutive etnries in 'signif' are TRUE
 check_signif_ind <- function(data, seq_len) {
@@ -15,16 +14,16 @@ check_signif_ind <- function(data, seq_len) {
   return(NA)
 }
 
+
 # Initialize some variables
 pronOnsetStep = DetOnsetStep = 3
 nSims <- 10000
 signif_seq_len <- 5 # no. of consecutive significant subSteps
 
 
-
-# Activation:
-
-# Condition 1 ---------
+# -------------------------------------------------------------
+#        Condition: MATCH
+# -------------------------------------------------------------
 
 act.cond1 <- read_csv('R-ACT-R/output-10k/cond-1/act-out-1.csv') %>%
   na.omit() %>%
@@ -119,7 +118,8 @@ input.stages.cond1 = c("<empty>", "klicke_auf", "pronoun", "<ant ret>",
                  "<pic pred>", "blauen", "<pic pred>", 
                  "knopf", "<pic pred>")
 
-# Plot fixation probabilities
+
+# Plot fixation probabilities ----
 ggplot(fix.prob.cond1, 
        aes(x=time, y=fix.prob, group=fix.prob.type)) +
   theme(panel.grid = element_blank(), axis.line=element_blank(),
@@ -150,7 +150,10 @@ ggplot(fix.prob.cond1,
 ggsave('MeanFixProb-cond-MATCH.png', plot = last_plot())
 
 
-# Condition 2 ---------
+
+# -------------------------------------------------------------
+#        Condition: MISMATCH
+# -------------------------------------------------------------
 
 act.cond2 <- read_csv('R-ACT-R/output-10k/cond-2/act-out-1.csv') %>%
   na.omit() %>%
@@ -212,7 +215,7 @@ onset.cond2 <- act.cond2 %>%
   pull()
 
 
-# Create fixation probabilities
+# Create fixation probabilities ----
 fix.prob.cond2 <- act.cond2 %>%
   # filter(stepNo > 2) %>% 
   # group_by(stepNo, subStep) %>% 
@@ -245,7 +248,8 @@ input.stages.cond2 = c("<empty>", "klicke_auf", "pronoun", "<ant ret>",
                        "<pic pred>", "blauen", "<pic pred>", 
                        "knopf", "<pic pred>")
 
-# Plot fixation probabilities
+
+# Plot fixation probabilities ----
 ggplot(fix.prob.cond2, 
        aes(x=time, y=fix.prob, group=fix.prob.type)) +
   theme(panel.grid = element_blank(), axis.line=element_blank(),
@@ -277,7 +281,9 @@ ggsave('MeanFixProb-cond-MISMATCH.png', plot = last_plot())
 
 
 
-# Condition 3 ---------
+# -------------------------------------------------------------
+#        Condition: BASELINE
+# -------------------------------------------------------------
 
 act.cond3 <- read_csv('R-ACT-R/output-10k/cond-3/act-out-1.csv') %>%
   na.omit() %>%
@@ -339,7 +345,7 @@ onset.cond3 <- act.cond3 %>%
   pull()
 
 
-# Create fixation probabilities
+# Create fixation probabilities ----
 fix.prob.cond3 <- act.cond3 %>%
   # filter(stepNo > 2) %>% 
   # group_by(stepNo, subStep) %>% 
@@ -373,7 +379,7 @@ input.stages.cond3 = c("<empty>", "klicke_auf", "determiner",
                        "knopf", "<pic pred>")
 
 
-# Plot fixation probabilities
+# Plot fixation probabilities ----
 ggplot(fix.prob.cond3, 
        aes(x=time, y=fix.prob, group=fix.prob.type)) +
   theme(panel.grid = element_blank(), axis.line=element_blank(),
@@ -401,236 +407,6 @@ ggplot(fix.prob.cond3,
                         labels = c("Flasche", "Knopf")) +
   geom_line(aes(linetype=fix.prob.type ))
 
-
 ggsave('MeanFixProb-cond-BASELINE.png', plot = last_plot())
-
-
-
-
-  
-
-
-
-
-
-
-
-
-`# Condition 2 ---------
-# act.cond2 <- read_csv('R-ACT-R/output-5k_v2/cond-2/act-out-1.csv') %>% 
-act.cond2 <- read_csv('R-ACT-R/output-10k/cond-2/act-out-1.csv') %>% 
-  na.omit()
-
-act.cond2 %>% 
-  group_by(stepNo, subStep) %>% 
-  summarise(time = mean(time), 
-            k.act = mean(knopf.act), 
-            f.act = mean(flasche.act)) %>% 
-  arrange(time) %>% 
-  print(n = Inf)
-
-act.cond2 %>% 
-  group_by(stepNo) %>% 
-  summarise(stepNoStart = min(time))
-
-
-# Generate fixations ----
-# 
-# Whichever of 'Knopf' and 'Flasche' has higher activation is
-# assumed to be fixated at that timestamp; if they are equal it's
-# decided randomly.
-# 
-# 'map_int()' is needed because otherwise sample() isn't called 
-# for each row, but only once since ifelse() evaluates for all rows
-act.cond2 <- act.cond2 %>%
-  mutate(knopf.fix = map_int(seq_len(n()), ~ {
-    if (knopf.act[.x] > flasche.act[.x]) {
-      return(1)
-    } else if (knopf.act[.x] < flasche.act[.x]) {
-      return(0)
-    } else {
-      return(sample(c(0, 1), 1))
-    }
-  })) %>% 
-  mutate(flasche.fix = 1- knopf.fix)
-
-# Sanity check
-act.cond2 %>% 
-  filter(knopf.act == flasche.act) %>% 
-  summarise(mean(knopf.fix), mean(flasche.fix))
-
-
-fix.prob.cond2 <- act.cond2 %>%
-  # filter(stepNo > 2) %>% 
-  # group_by(stepNo, subStep) %>% 
-  group_by(stepNo) %>% 
-  summarise(time = mean(time), 
-            k.fix.prob = mean(knopf.fix), 
-            f.fix.prob = mean(flasche.fix)) %>% 
-  arrange(time) %>% 
-  pivot_longer(
-    cols = c("k.fix.prob", "f.fix.prob"), 
-    names_to = "fix.prob.type", 
-    values_to = "fix.prob"
-  )
-
-
-subSteps <- act.cond2 %>%
-  group_by(stepNo, subStep) %>%
-  summarise(subSteptime = mean(time)) %>% 
-  arrange(subSteptime)
-
-steps <- subSteps %>% 
-  group_by(stepNo) %>% 
-  summarise(stepStartTime = min(subSteptime))
-
-pronOnsetStep = DetOnsetStep = 3
-pronOnset = steps %>% 
-  filter(stepNo == pronOnsetStep) %>% 
-  pull(stepStartTime)
-
-input.stages.cond2 = c("<empty>", "klicke_auf", "pronoun", "<ant ret>", 
-                       "<pic pred>", "blauen", "<pic pred>", 
-                       "knopf", "<pic pred>")
-
-# Plot fixation probabilities
-ggplot(fix.prob.cond2, 
-       aes(x=time, y=fix.prob, group=fix.prob.type)) +
-  theme(panel.grid = element_blank(), axis.line=element_blank(),
-        panel.background=element_blank(), 
-        plot.title = element_text(hjust = 0.5)) +
-  xlim(0,2000) +
-  ylim(0,1) +
-  geom_point() +
-  ylab('') +
-  xlab('Time') +
-  ggtitle('Mismatch') +
-  geom_vline(xintercept = subSteps$subSteptime, 
-             linetype="dotted", color = "gray70") +
-  geom_vline(xintercept = steps$stepStartTime,
-             color = "gray80", ) +
-  annotate("text", x=steps$stepStartTime, y=0, 
-           label= input.stages.cond2, angle=90, hjust = 0) +
-  annotate("text", x=subSteps$subSteptime, y=0.9, size = 2.5, 
-           label= subSteps$subStep, angle=90, hjust = 0) +
-  # geom_vline(xintercept = pronOnset, color = "gray45") +
-  scale_linetype_manual(name = "", 
-                        values = c("dashed", "solid"), 
-                        labels = c("Flasche", "Knopf")) +
-  geom_line(aes(linetype=fix.prob.type ))
-
-
-
-
-
-# Condition 3 ---------
-# act.cond3 <- read_csv('R-ACT-R/output-5k_v2/cond-3/act-out-1.csv') %>% 
-act.cond3 <- read_csv('R-ACT-R/output-10k/cond-3/act-out-1.csv') %>% 
-  na.omit()
-
-act.cond3 %>% 
-  group_by(stepNo, subStep) %>% 
-  summarise(time = mean(time), 
-            k.act = mean(knopf.act), 
-            f.act = mean(flasche.act)) %>% 
-  arrange(time) %>% 
-  print(n = Inf)
-
-act.cond3 %>% 
-  group_by(stepNo) %>% 
-  summarise(stepNoStart = min(time))
-
-
-# Generate fixations
-
-# 'map_int()' is needed because otherwise sample() isn't called 
-# for each row, but only once since ifelse() evaluates for all rows
-act.cond3 <- act.cond3 %>%
-  mutate(knopf.fix = map_int(seq_len(n()), ~ {
-    if (knopf.act[.x] > flasche.act[.x]) {
-      return(1)
-    } else if (knopf.act[.x] < flasche.act[.x]) {
-      return(0)
-    } else {
-      return(sample(c(0, 1), 1))
-    }
-  })) %>% 
-  mutate(flasche.fix = 1- knopf.fix)
-
-# Sanity check
-act.cond3 %>% 
-  filter(knopf.act == flasche.act) %>% 
-  summarise(mean(knopf.fix), mean(flasche.fix))
-
-
-fix.prob.cond3 <- act.cond3 %>%
-  # filter(stepNo > 2) %>% 
-  # group_by(stepNo, subStep) %>% 
-  group_by(stepNo) %>% 
-  summarise(time = mean(time), 
-            k.fix.prob = mean(knopf.fix), 
-            f.fix.prob = mean(flasche.fix)) %>% 
-  arrange(time) %>% 
-  pivot_longer(
-    cols = c("k.fix.prob", "f.fix.prob"), 
-    names_to = "fix.prob.type", 
-    values_to = "fix.prob"
-  )
-
-
-subSteps <- act.cond3 %>%
-  group_by(stepNo, subStep) %>%
-  summarise(subSteptime = mean(time)) %>% 
-  arrange(subSteptime)
-
-steps <- subSteps %>% 
-  group_by(stepNo) %>% 
-  summarise(stepStartTime = min(subSteptime))
-
-pronOnsetStep = DetOnsetStep = 3
-pronOnset = steps %>% 
-  filter(stepNo == pronOnsetStep) %>% 
-  pull(stepStartTime)
-
-input.stages.cond3 = c("<empty>", "klicke_auf", "determiner", 
-                       "<pic pred>", "blauen", "<pic pred>", 
-                       "knopf", "<pic pred>")
-
-# Plot fixation probabilities
-ggplot(fix.prob.cond3, 
-       aes(x=time, y=fix.prob, group=fix.prob.type)) +
-  theme(panel.grid = element_blank(), axis.line=element_blank(),
-        panel.background=element_blank(), 
-        plot.title = element_text(hjust = 0.5)) +
-  xlim(0,2000) +
-  ylim(0,1) +
-  geom_point() +
-  ylab('') +
-  xlab('Time') +
-  ggtitle('Baseline') +
-  geom_vline(xintercept = subSteps$subSteptime, 
-             linetype="dotted", color = "gray70") +
-  geom_vline(xintercept = steps$stepStartTime,
-             color = "gray80", ) +
-  annotate("text", x=steps$stepStartTime, y=0, 
-           label= input.stages.cond3, angle=90, hjust = 0) +
-  annotate("text", x=subSteps$subSteptime, y=0.9, size = 2.5, 
-           label= subSteps$subStep, angle=90, hjust = 0) +
-  # geom_vline(xintercept = pronOnset, color = "gray45") +
-  scale_linetype_manual(name = "", 
-                        values = c("dashed", "solid"), 
-                        labels = c("Flasche", "Knopf")) +
-  geom_line(aes(linetype=fix.prob.type ))
-
-
-
-
-
-
-
-
-
-
-
 
 
